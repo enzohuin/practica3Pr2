40074f4a96bf559cb1153f44055f25029398135c
enzohuin <133805188+enzohuin@users.noreply.github.com>
2024-03-19 11:05:41 +0100

    Delete practica3Pr2 directory

--- practica3Pr2/src/aplicacion/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- *Este paquete contiene la clase principal del programa y se encarga de mostrar los resultados de las aproximaciones de Pi.
- *Es la interfaz de usuario para utilizar las funciones de la clase Matematicas
- */
-  package aplicacion;
